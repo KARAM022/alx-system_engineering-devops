bf5435d49bd79b0e82f3c369c0691ab350e20082
KARAM022 <my0fav0library@gmail.com>
2024-04-19 18:04:08 +0100

    0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,5 +6,6 @@ packege { 'python3-pip3':
 exec { 'install_flask':
     command => '/usr/bin/pip3 install Flask==2.1.0',
     path    => ['/usr/bin'],
+    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
     require => Package['python3-pip'],
 }
