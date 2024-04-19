4ef1b983cbd3c09caae2e92702be81e03d8dd910
KARAM022 <my0fav0library@gmail.com>
2024-04-19 18:02:39 +0100

    0x0A

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# CMNT
+packege { 'python3-pip3':
+    ensure => installed,
+}
+
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => ['/usr/bin'],
+    require => Package['python3-pip'],
+}
